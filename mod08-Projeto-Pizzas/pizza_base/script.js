15874cfba01049def9d3245a6b98e65be7783dff
Betoside <betoside@gmail.com>
2022-08-29 18:43:04 -0300

    passo 11: funcao que vai abrir ou fechar o carrinho

--- mod08-Projeto-Pizzas/pizza_base/script.js
+++ mod08-Projeto-Pizzas/pizza_base/script.js
@@ -111,8 +111,22 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     }
 
     console.log(cart);
-
+    updateCart();
     closeModal();
-
 });
 
+function updateCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item)=>{
+                return item.id == cart[i].id;
+
+                console.log(pizzaItem);
+            });
+        }
+    } else {
+        c('aside').classList.remove('show');
+    }
+}
