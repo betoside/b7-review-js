c5a8251f67dd38df2cd5ae338ac38ce9d999199f
Betoside <betoside@gmail.com>
2022-08-29 03:09:52 -0300

    passo 7: fechar modal. passo 8: bt aumentar/diminuir e bts escolher o tamanho

--- mod08-Projeto-Pizzas/pizza_base/script.js
+++ mod08-Projeto-Pizzas/pizza_base/script.js
@@ -3,6 +3,7 @@ let modalqt = 1;
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
+// listagem das pizzas
 pizzaJson.map((item, index)=>{
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -44,4 +45,36 @@ pizzaJson.map((item, index)=>{
 
     c('.pizza-area').append(pizzaItem);
 
+});
+
+// eventos do modal
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click',()=>{
+    if(modalqt>1){
+        modalqt--;
+        c('.pizzaInfo--qt').innerHTML = modalqt;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click',()=>{
+    modalqt++;
+    c('.pizzaInfo--qt').innerHTML = modalqt;
+});
+
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click',(e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
 });
