06cca573584514282cd3f311eb0c144b1871456e
Betoside <betoside@gmail.com>
2022-09-02 01:17:59 -0300

    Desafio 7 proj em 7 dias: 02 relogio. Finalizado. pegamos o date atual, calculo de hora, minuto, segundo, uma função para rodar a cada segundo, pah

---
+++ mod13-Projeto02-relogio/relogio/script.js
@@ -0,0 +1,30 @@
+seletor = (e)=>document.querySelector(e);
+
+let digitalElement = seletor('.digital');
+let sElement = seletor('.p_s');
+let mElement = seletor('.p_m');
+let hElement = seletor('.p_h');
+
+function updateClock(){
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let sDeg = ((360/60) * second) - 90;
+    let mDeg = ((360/60) * minute) - 90;
+    let hDeg = ((360/12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fixZero(time){
+    return time<10 ? '0'+time : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
